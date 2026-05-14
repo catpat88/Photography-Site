0d0af06d95c85455ac961a8502fb436bd1b9e3dc
Your Name <you@example.com>
2026-05-14 14:25:36 +0100

    update speed changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -97,3 +97,24 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// Reviews slider 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("testimonial-item");
+    
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    
+    // Show the current slide
+    slides[slideIndex-1].style.display = "block";  
+    
+    setTimeout(showSlides, 8000); // Change review every 8 seconds
+}
+
